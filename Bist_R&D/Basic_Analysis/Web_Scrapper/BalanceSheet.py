5a3b6fc663089ccaaba56458521611c78b75f1ef
Furkan Ozgel <furkan.ozgel@hotmail.com>
2023-08-13 16:50:20 +0300

    Added Historical Balance Sheet Feature

--- Bist_R&D/Basic_Analysis/Web_Scrapper/BalanceSheet.py
+++ Bist_R&D/Basic_Analysis/Web_Scrapper/BalanceSheet.py
@@ -5,7 +5,7 @@ from lxml import etree
 from selenium import webdriver
 import time
 import unicodedata
-
+import numpy as np
 
 def run_driver():
     global driver
@@ -84,6 +84,102 @@ def get_financal_tables(stock, year, month):
 
     return df
 
+
+def get_historical_financal_tables(stock):
+    stock = "odas"
+    firs_table_items = np.array([0 ,0 ,0 ,0])
+    second_table_items = np.array([0 ,0 ,0 ,0])
+    third_table_items = np.array([0 ,0 ,0 ,0])
+    table_items = []
+    table_item_codes = []
+    columns = ["ItemCode"] + get_finance_data_dates(stock)[:12]
+
+    url = "https://www.isyatirim.com.tr/_layouts/15/IsYatirim.Website/Common/Data.aspx/MaliTablo"
+
+    main_url = "https://www.isyatirim.com.tr/tr-tr/analiz/hisse/Sayfalar/sirket-karti.aspx?hisse=" + stock
+    grup_respone = requests.get(main_url)
+    soup = BeautifulSoup(grup_respone.text, "html.parser")
+
+    choice = soup.find("select", id="ddlMaliTabloGroup")
+    grup = choice.find("option")["value"]
+
+    if (grup == "UFRS"):
+        grup = "UFRS_K"
+
+    parametreler = [
+        (
+        ("companyCode", stock),
+        ("exchange", "TRY"),
+        ("financialGroup", grup),
+        ("year1", get_finance_data_dates(stock)[0].split("/")[0]),
+        ("period1", get_finance_data_dates(stock)[0].split("/")[1]),
+        ("year2", get_finance_data_dates(stock)[1].split("/")[0]),
+        ("period2", get_finance_data_dates(stock)[1].split("/")[1]),
+        ("year3", get_finance_data_dates(stock)[2].split("/")[0]),
+        ("period3", get_finance_data_dates(stock)[2].split("/")[1]),
+        ("year4", get_finance_data_dates(stock)[3].split("/")[0]),
+        ("period4", get_finance_data_dates(stock)[3].split("/")[1]),
+        ),
+        (
+        ("companyCode", stock),
+        ("exchange", "TRY"),
+        ("financialGroup", grup),
+        ("year1", get_finance_data_dates(stock)[4].split("/")[0]),
+        ("period1", get_finance_data_dates(stock)[4].split("/")[1]),
+        ("year2", get_finance_data_dates(stock)[5].split("/")[0]),
+        ("period2", get_finance_data_dates(stock)[5].split("/")[1]),
+        ("year3", get_finance_data_dates(stock)[6].split("/")[0]),
+        ("period3", get_finance_data_dates(stock)[6].split("/")[1]),
+        ("year4", get_finance_data_dates(stock)[7].split("/")[0]),
+        ("period4", get_finance_data_dates(stock)[7].split("/")[1]),
+        ),
+        (
+        ("companyCode", stock),
+        ("exchange", "TRY"),
+        ("financialGroup", grup),
+        ("year1", get_finance_data_dates(stock)[8].split("/")[0]),
+        ("period1", get_finance_data_dates(stock)[8].split("/")[1]),
+        ("year2", get_finance_data_dates(stock)[9].split("/")[0]),
+        ("period2", get_finance_data_dates(stock)[9].split("/")[1]),
+        ("year3", get_finance_data_dates(stock)[10].split("/")[0]),
+        ("period3", get_finance_data_dates(stock)[10].split("/")[1]),
+        ("year4", get_finance_data_dates(stock)[11].split("/")[0]),
+        ("period4", get_finance_data_dates(stock)[11].split("/")[1]),
+        )
+        ]
+
+    response = [
+        requests.get(url, params=parametreler[0]).json()["value"],
+        requests.get(url, params=parametreler[1]).json()["value"],
+        requests.get(url, params=parametreler[2]).json()["value"]
+    ]
+
+    for i in response[0]:
+        row_values = np.array([i["value1"], i["value2"], i["value3"], i["value4"]])  
+        firs_table_items = np.vstack((firs_table_items, row_values))
+        table_items.append(i["itemDescTr"])
+        table_item_codes.append(i["itemCode"])
+        
+    for i in response[1]:
+        row_values = np.array([i["value1"], i["value2"], i["value3"], i["value4"]])  
+        second_table_items = np.vstack((second_table_items, row_values))
+        
+    for i in response[2]:
+        row_values = np.array([i["value1"], i["value2"], i["value3"], i["value4"]])  
+        third_table_items = np.vstack((third_table_items, row_values))  
+
+    table_item_codes = np.array(table_item_codes).reshape(147, 1)
+
+    firs_table_items = firs_table_items[1:, :]
+    second_table_items = second_table_items[1:, :]
+    third_table_items = third_table_items[1:, :]
+
+    merged_np_array = np.hstack((firs_table_items, second_table_items, third_table_items))
+    merged_np_array = np.concatenate((table_item_codes, merged_np_array), axis=1)
+
+    df = pd.DataFrame(merged_np_array ,index=table_items, columns=columns)
+    
+    return df
 
 def get_ready_ratio(stock, path):
     url = "https://www.isyatirim.com.tr/tr-tr/analiz/hisse/Sayfalar/sirket-karti.aspx?hisse="+stock
