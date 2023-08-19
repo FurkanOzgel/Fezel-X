63af34d82cd852dc303c05d4c2659a27921759f4
Furkan Ozgel <furkan.ozgel@hotmail.com>
2023-08-19 19:00:41 +0300

    Added spyder file for variable explorer

---
+++ data/spyder.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+
+import pandas as pd
+
+sector_average_df = pd.read_csv("sector_average_df.csv", index_col=0)
+share_ratio_df = pd.read_csv("share_ratio_df.csv", index_col=0)
+#share_sector_df = pd.read_csv("share_sector_df.csv")
